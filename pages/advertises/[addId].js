2feedd726baf7352284005f477abb75a211ae663
MeRaj-Rudba <mrudba@gmail.com>
2022-01-30 10:35:06 +0600

    Second Try

--- pages/advertises/[addId].js
+++ pages/advertises/[addId].js
@@ -1,10 +1,8 @@
-import { ObjectId } from "mongodb";
+// import { ObjectId } from "mongodb";
 import Head from "next/head";
 import React, { Fragment } from "react";
 import AddDetails from "../../components/details/add-details";
-import { connectToDatabase } from "../../lib/db";
-
-import logo from "../../public/images/logo2.png";
+// import { connectToDatabase } from "../../lib/db";
 
 export default function AdvertisePage(props) {
   return (
@@ -22,16 +20,16 @@ export default function AdvertisePage(props) {
 }
 
 export async function getStaticPaths() {
-  const client = await connectToDatabase();
-  const db = client.db();
-  const postData = await db.collection("posts").find().toArray();
+  // const client = await connectToDatabase();
+  // const db = client.db();
+  // const postData = await db.collection("posts").find().toArray();
 
-  const posts = JSON.parse(JSON.stringify(postData));
+  // const posts = JSON.parse(JSON.stringify(postData));
 
-  // const res = await fetch(`http://localhost:5000/post/posts`);
-  // const posts = await res.json();
+  const res = await fetch(`http://localhost:5000/post/posts`);
+  const posts = await res.json();
 
-  const paths = posts.map((p) => ({
+  const paths = posts.data.map((p) => ({
     params: { addId: p._id },
   }));
   // console.log("Paths", paths);
@@ -44,25 +42,25 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const addId = context.params.addId;
-  const client = await connectToDatabase();
-  const db = client.db();
-  const postData = await db
-    .collection("posts")
-    .find({
-      _id: ObjectId(addId),
-    })
-    .toArray();
+  // const client = await connectToDatabase();
+  // const db = client.db();
+  // const postData = await db
+  //   .collection("posts")
+  //   .find({
+  //     _id: ObjectId(addId),
+  //   })
+  //   .toArray();
 
-  const posts = JSON.parse(JSON.stringify(postData));
-  const post = posts[0];
+  // const posts = JSON.parse(JSON.stringify(postData));
+  // const post = posts[0];
 
-  // const res = await fetch(`http://localhost:5000/post/posts/${addId}`);
-  // const post = await res.json();
+  const res = await fetch(`http://localhost:5000/post/posts/${addId}`);
+  const post = await res.json();
 
   return {
     props: {
-      post: post,
-      // post: post.data,
+      // post: post,
+      post: post.data,
     },
     revalidate: 1,
   };
