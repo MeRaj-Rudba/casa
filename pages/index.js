7f5afc8e05ff1ee24b616ef4fab91305f84665bc
MeRaj-Rudba <mrudba@gmail.com>
2022-02-01 10:33:30 +0600

    4th

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import FeaturedPosts from "../components/home/featured-posts";
-import { connectToDatabase } from "../lib/db";
 
-export default function Home(props) {
+export default function Home({ posts }) {
   return (
     <div className="page bg-gray-900">
       <Head>
@@ -14,31 +13,19 @@ export default function Home(props) {
       <h1 className="max-w-7xl mx-auto p-10 pb-0 font-semibold text-3xl text-gray-200">
         Featured
       </h1>
-      <FeaturedPosts posts={props.posts} />
+      <FeaturedPosts posts={posts} />
     </div>
   );
 }
 
 export async function getStaticProps() {
-  // Fetch data from external API
-  // const res = await fetch("http://localhost:5000/post/posts", {
-  //   method: "GET",
-  //   headers: {
-  //     // update with your user-agent
-  //     "User-Agent":
-  //       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-  //     Accept: "application/json; charset=UTF-8",
-  //   },
-  // });
-  // const posts = await res.json();
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch("http://localhost:5000/post/posts");
+  const posts = await res.json();
 
-  const client = await connectToDatabase();
-  const db = client.db();
-  const postsData = await db.collection("posts").find().toArray();
-
-  const posts = JSON.parse(JSON.stringify(postsData));
-
-  // Pass data to the page via props
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
   return {
     props: {
       posts: posts.filter((p) => {
@@ -48,5 +35,6 @@ export async function getStaticProps() {
         return false;
       }),
     },
+    revalidate: 10,
   };
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import React, { Fragment } from "react";
 import FeaturedPosts from "../../components/home/featured-posts";
-// import { connectToDatabase } from "../../lib/db";
 
-export default function AdvertisePage(props) {
+export default function AdvertisePage({ posts }) {
   return (
     <div className="page bg-gray-900">
       <Head>
@@ -14,31 +13,22 @@ export default function AdvertisePage(props) {
       <h1 className="max-w-7xl mx-auto p-10 pb-0 font-semibold text-3xl text-gray-200">
         All Advertises
       </h1>
-      <FeaturedPosts posts={props.posts} />
+      <FeaturedPosts posts={posts} />
     </div>
   );
 }
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch("http://localhost:5000/post/posts");
+  const posts = await res.json();
 
-export async function getServerSideProps() {
-  const res = await fetch(`http://localhost:5000/post/posts`);
-  const data = await res.json();
-
-  // Pass data to the page via props
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
   return {
     props: {
-      posts: data,
+      posts,
     },
+    revalidate: 10,
   };
-
-  // const client = await connectToDatabase();
-  // const db = client.db();
-  // const postsData = await db.collection("posts").find().toArray();
-
-  // const posts = JSON.parse(JSON.stringify(postsData));
-  // // Pass data to the page via props
-  // return {
-  //   props: {
-  //     posts: posts,
-  //   },
-  // };
 }
