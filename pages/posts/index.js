9b5ff3564767e047f53715c32c316198478b8223
MeRaj-Rudba <mrudba@gmail.com>
2022-02-01 10:27:04 +0600

    3rdd

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -19,7 +19,7 @@ export default function AdvertisePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const res = await fetch(`http://localhost:5000/post/posts`);
   const data = await res.json();
 
